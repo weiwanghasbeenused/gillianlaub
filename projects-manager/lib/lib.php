e520bc392b8e78e31bca6d9abae3acf6da7eba6b
wei <wei@o-r-g.com>
2022-11-28 11:13:05 +0800

    move lib.php

---
+++ projects-manager/lib/lib.php
@@ -0,0 +1,10 @@
+<?
+function trimBreaksFromSides($string){
+    if(!empty($string))
+    {
+        $output = str_replace('<br>', "\n\r", $string);
+        $output = trim($output);
+    }
+    else $output = '';
+    return $output;
+} 
